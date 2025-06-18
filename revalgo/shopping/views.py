283c81b0b7f7e6231ae571fb443cca93bd80c1af
Karri Tirumala Venkatesh <ktirumalavenkatesh@gmail.com>
2025-06-18 14:56:59 +0530

    Changes Made

---
+++ revalgo/shopping/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    """
+    Render the index page of the shopping app.
+    
+    Args:
+        request: The HTTP request object.
+    
+    Returns:
+        HttpResponse: Rendered index page.
+    """
+    return render(request, 'shopping/index.html')
